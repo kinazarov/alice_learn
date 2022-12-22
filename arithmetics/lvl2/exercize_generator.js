9b37b3cc01a8c26f3013a25928ad1830411c43b8
kinazarov <kinazarov@gmail.com>
2022-12-22 13:19:04 +0200

    lvl2 ready

--- arithmetics/lvl2/exercize_generator.js
+++ arithmetics/lvl2/exercize_generator.js
@@ -1,4 +1,8 @@
-const PARTS_COUNT = 4;
+import clipboardy from 'clipboardy'
+
+const PARTS_COUNT = 3;
+const EXERSISE_COUNT = 7;
+const EXERSISES_ON_PAGE = 24;
 
 
 function randomNumber_100() {
@@ -38,7 +42,7 @@ function get_part(limit) {
             if(multiplication){
                 let m1 = multiplier()
                 let m2 = multiplier()
-                result.representation = `${m1} · ${m2}`
+                result.representation = `${m1}·${m2}`
                 result.value = m1* m2
             } else {
                 result.value = Math.floor(Math.random() * (limit - 1)) + 1
@@ -51,8 +55,45 @@ function get_part(limit) {
 }
 
 
-
-for(let part_index = 0; part_index <= PARTS_COUNT ; part_index++){
-    let n = get_part()
-    console.log('◩◩◩◩◩◩ ', JSON.stringify(n));
+function getSign(seed){
+    seed = seed ? seed : randomNumber_100() < 50 ? -1 : 1
+    return {
+        representation: seed > 0 ? "+" : "-",
+        value: seed
+    }
 }
+ 
+function get_exercise(){
+    let initial_part = get_part()
+    let parts = [initial_part]
+    let intermediate_result = initial_part.value
+
+    for(let part_index = 1; part_index <= PARTS_COUNT ; part_index++){
+        let current_part = get_part()
+        let sign = null
+        if(current_part.value > intermediate_result){
+            sign = getSign(1)
+        } else {
+            sign = getSign(randomNumber_100() < 70 ? -1 : 1)
+        }
+
+        parts.push(sign)
+        parts.push(current_part)
+
+        intermediate_result = intermediate_result + sign.value * current_part.value
+    }
+
+    return parts
+}
+
+let block = '';
+
+for(let exercise_block_index = 0; exercise_block_index < EXERSISES_ON_PAGE; exercise_block_index++){
+    for(let exercise_index = 0; exercise_index < EXERSISE_COUNT; exercise_index++){
+        let exercise_string = get_exercise().reduce((acc, part) => acc = acc + " " + part.representation, '')
+        block = block + `${exercise_index + 1}. ${exercise_string}\n`;
+    }
+    block = block + '\n\n';
+}
+
+clipboardy.writeSync(block)
